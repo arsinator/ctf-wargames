dac09f7bda1189ed3e42114f5effde845519fb2c
napster23 <b9125@yandex.ru>
2019-04-27 13:30:52 +0300

    ctr

--- cryptopals/blockAndStream/ctr.py
+++ cryptopals/blockAndStream/ctr.py
@@ -1,3 +1,4 @@
+from math import ceil
 from base64 import b64decode
 from base64 import b64encode
 from random import randint
@@ -8,7 +9,13 @@ from CBCmodeAes import unPad, pk7, aesCbcEnc, aesCbcDec, aesEnc,xor
 cr = b64decode("L77na/nrFsKvynd6HzOoG7GHTLXsTVu9qvY/2syLXzhPweyyMTJULu/6/kXX0KSvoOLSFQ==")
 print(len(cr))
 u=aesEnc("\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00","YELLOW SUBMARINE")+aesEnc("\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00","YELLOW SUBMARINE")+aesEnc("\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00","YELLOW SUBMARINE")
-
+def ctrEncr(txt,key):
+	nonce = bytearray("\x00"*16,"latin1")
+	res = bytearray()
+	for i in range(0,ceil(len(txt)/16)):
+		nonce[8]=i
+		res += aesEnc(nonce.decode("latin1"),key)
+	return xor(res,txt)
 print(xor(cr,u))
 
-
+print(ctrEncr(cr,"YELLOW SUBMARINE"))
